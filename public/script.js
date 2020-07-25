cab9ae7e768e9d2f8fb2b88438b80372dedcf8de
Shubham Jain <shubhamjain2908@gmail.com>
2020-07-26 01:46:22 +0530

    connectToNewUser

--- public/script.js
+++ public/script.js
@@ -12,14 +12,14 @@ navigator.mediaDevices.getUserMedia({
     audio: true
 }).then(stream => {
     addVideoStream(myVideo, stream);
+
+    socket.on('user-connected', userId => {
+        connectToNewUser(userId, stream);
+    });
 });
 
 myPeer.on('open', id => {
     socket.emit('join-room', ROOM_ID, id);
-});
-
-socket.on('user-connected', userId => {
-    console.log("User connected: ", userId);
 });
 
 function addVideoStream(video, stream) {
@@ -28,4 +28,15 @@ function addVideoStream(video, stream) {
         video.play();
     });
     videoGrid.append(video);
+}
+
+function connectToNewUser(userId, stream) {
+    const call = myPeer.call(userId, stream);
+    const video = document.createElement('video');
+    call.on('stream', userVideoStream => {
+        addVideoStream(video, userVideoStream);
+    });
+    call.on('close', () => {
+        video.remove();
+    })
 }
