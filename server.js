92ac4300332385120e02410ed56ba84b526ca06f
Shubham Jain <shubhamjain2908@gmail.com>
2020-07-26 01:03:32 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`App running on port: ${PORT}`));
